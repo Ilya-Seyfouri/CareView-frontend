b66f2c6bbfeeaafb71c633db3cdf41ae45062366
iseyf <c4033962@newcastle.ac.uk>
2025-08-01 19:06:46 +0100

    trying to get rid of // error

--- src/pages/managers/ManagerDashboard.jsx
+++ src/pages/managers/ManagerDashboard.jsx
@@ -16,7 +16,7 @@ export default function ManagerDashboard() {
     const fetchDashboardData = async () => {
       try {
         setIsLoading(true)
-        const data = await apiGet('/manager/dashboard')
+        const data = await apiGet('manager/dashboard')
         setDashboardData(data)
         setError(null)
       } catch (err) {
